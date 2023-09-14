cca378de6c3a766a2fe15e143b0f7f9ed89945e4
ircamitoc <iraincamitoc@gmail.com>
2023-09-15 02:24:54 +0800

    Updated files

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { db } = require("./db/db");
+const { readdirSync } = require("fs");
 require("dotenv").config();
 
 const PORT = process.env.PORT; // Use a default port (e.g., 3000) if PORT is not defined in your .env file.
@@ -12,6 +13,14 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// routes
+readdirSync("./routes").map((route) => {
+  if (route !== "index.js") {
+    // Exclude the main index.js file from being used as a route
+    app.use("/api/v1", require(`./routes/${route}`));
+  }
 });
 
 const server = () => {
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,9 @@
-const router = require("express").router;
+const express = require("express");
+const router = express.Router(); // Create a router instance, not an app instance
 
+// Define routes using the router
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello from transactions route!");
 });
 
 module.exports = router;
