0d22bd6df60b23972a984ec71040db9843e8452d
ircamitoc <iraincamitoc@gmail.com>
2023-09-15 03:13:33 +0800

    Initialize files

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,9 +1,22 @@
-const express = require("express");
-const router = express.Router(); // Create a router instance, not an app instance
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
 
-// Define routes using the router
-router.get("/", (req, res) => {
-  res.send("Hello from transactions route!");
-});
+const router = require("express").Router();
+
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
