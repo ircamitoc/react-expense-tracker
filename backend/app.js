fc380e3ebd05aa3c8cbc0574dffa838afbf4de43
ircamitoc <iraincamitoc@gmail.com>
2023-09-15 02:09:31 +0800

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { db } = require("./db/db");
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; // Use a default port (e.g., 3000) if PORT is not defined in your .env file.
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const server = () => {
-  console.log("You're listening to port:", PORT);
+  db();
+  app.listen(PORT, () => {
+    console.log(`Listening to port`, PORT);
+  });
 };
 
 server();
