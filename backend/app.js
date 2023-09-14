6ebae3b30bde26fa70daf093b7c58fbf9b242e89
ircamitoc <iraincamitoc@gmail.com>
2023-09-15 03:47:01 +0800

    Initialize files

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const { db } = require("./db/db");
-const { readdirSync } = require("fs");
-require("dotenv").config();
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
 
-const PORT = process.env.PORT; // Use a default port (e.g., 3000) if PORT is not defined in your .env file.
+require('dotenv').config()
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
+const PORT = process.env.PORT
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//middlewares
+app.use(express.json())
+app.use(cors())
 
-// routes
-readdirSync("./routes").map((route) => {
-  if (route !== "index.js") {
-    // Exclude the main index.js file from being used as a route
-    app.use("/api/v1", require(`./routes/${route}`));
-  }
-});
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
-  db();
-  app.listen(PORT, () => {
-    console.log(`Listening to port`, PORT);
-  });
-};
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
 
-server();
+server()
